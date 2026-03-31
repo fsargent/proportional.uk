46d07cc4e70efb68b6ed0d1cd9bf6af7d454eaaf
Felix Sargent <felix.sargent@gmail.com>
2026-03-31 13:09:45 +0100

    Use Lefthook for pre-push checks

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+	allowedHosts: ["shell.tail1901c.ts.net"]
+}
 });
